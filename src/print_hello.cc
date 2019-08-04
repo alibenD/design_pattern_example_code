864c57a1578d56327ea94cdc13b271ad23ebdf24
alibenD <aliben.develop@gmail.com>
2019-08-04 19:25:51 +0800

    Init project

---
+++ src/print_hello.cc
@@ -0,0 +1,19 @@
+/**-----------------------------------------------
+  * @Copyright (C) All rights reserved.
+  * @filename: design_pattern
+  * @author: aliben.develop@gmail.com
+  * @created_date: 2019-08-04 19:25:51
+  * @last_modified_date: NO_LAST_MODIFIED_DATE
+  * @description: TODO
+  *-----------------------------------------------*/
+
+//INCLUDE
+#include <design_pattern/print_hello.hh>
+
+//CODE
+
+int print_hello()
+{
+  std::cout << "Hello world. -- Aliben auto create." << std::endl;
+  return 0;
+}
