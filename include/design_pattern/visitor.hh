7c3c72f32ebe5ca4a4b9c77494949a7199ebeca9
alibenD <aliben.develop@gmail.com>
2019-08-07 09:56:12 +0800

    Close first version

---
+++ include/design_pattern/visitor.hh
@@ -0,0 +1,18 @@
+#ifndef __VISITOR_HH__
+#define __VISITOR_HH__
+/**-----------------------------------------------
+  * @Copyright (C) 2019 All rights reserved.
+  * @date: 2019
+  * @file: visitor.hh
+  * @version: v0.0.1
+  * @author: aliben.develop@gmail.com
+  * @create_date: 2019-08-07 09:04:39
+  * @last_modified_date: 2019-08-07 09:04:42
+  * @brief: TODO
+  * @details: TODO
+  *-----------------------------------------------*/
+
+// Header include
+
+// Declaration
+#endif // __VISITOR_HH__
---
+++ src/visitor.cc
@@ -0,0 +1,17 @@
+/**
+  * @Copyright (C) 2019 All rights reserved.
+  * @date: 2019
+  * @file: visitor.cc
+  * @version: v0.0.1
+  * @author: aliben.develop@gmail.com
+  * @create_date: 2019-08-07 09:04:39
+  * @last_modified_date: 2019-08-07 09:05:04
+  * @brief: TODO
+  * @details: TODO
+  */
+
+//INCLUDE
+#include <design_pattern/visitor.hh>
+
+//CODE
+
